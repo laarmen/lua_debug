0b3f5ccd6acb9e6a27fe76091a470b3198457637
Simon Chopin <chopin.simon@gmail.com>
2012-12-13 16:10:06 +0100

    Add tests for the breakpoints

--- tests.lua
+++ tests.lua
@@ -1,9 +1,10 @@
-
+debug.breakpoints['tests.lua'] = {5, 10}
 i = 10
 f = function (a, b)
     local c = "test"
-    for ii = 1,100000,1 do
-        if ii%10 == 0 then
+
+    for ii = 1,10000,1 do
+        if ii%1000 == 0 then
             print(ii)
         end
     end
