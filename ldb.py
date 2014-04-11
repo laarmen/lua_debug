5679d514c9e6d0b985a2f64bc81138d41e917d14
Simon Chopin <chopin.simon@gmail.com>
2014-04-12 00:14:55 +0200

    Add rough console support

--- ldb.py
+++ ldb.py
@@ -8,6 +8,7 @@
 
 from socket import socket, AF_UNIX, SOCK_STREAM
 import os
+import sys
 import time
 
 class Ldb(object):
@@ -15,7 +16,7 @@ class Ldb(object):
         self._lua = conn
         self._parent = parent
 
-    def __del__(self):
+    def teardown(self):
         self._lua.close()
         self._parent.close_conn(self)
 
@@ -35,6 +36,7 @@ class Ldb(object):
     def get(self, var_name):
         self._lua.send("get_var {0}".format(var_name))
         answer = self._lua.recv(1024)
+        print answer
         return answer
 
     def break_(self, filename, line_nb):
@@ -63,18 +65,58 @@ class Ldb(object):
         self._lua.send("ACK end")
         return bt
 
+class Console(object):
+    def __init__(self, ldb):
+        self.ldb = ldb
+        self.cmds = {
+                "run": ldb.run,
+                "start": ldb.start,
+                "close": lambda: ldb.close(ldb.current()),
+                "next_context": ldb.next_context,
+                "continue": lambda: ldb.current().continue_(),
+                "c": lambda: ldb.current().continue_(),
+                "backtrace": lambda: ldb.current().backtrace(),
+                "bt": lambda: ldb.current().backtrace(),
+                "up": lambda: ldb.current().up(),
+                "down": lambda: ldb.current().down(),
+                "break": lambda f, n: ldb.current().break_(f, n),
+                "b": lambda f, n: ldb.current().break_(f, n),
+                "print": lambda var: ldb.current().get(var),
+        }
+
+    def run(self):
+        while True:
+            try:
+                self.prompt()
+            except Exception as e:
+                print e
+
+    def prompt(self):
+        sys.stdout.write("> ")
+        cmd = sys.stdin.readline().split()
+        self.cmds[cmd[0]](*tuple(cmd[1:]))
+
 class LdbServer(object):
     def __init__(self, sock_path='/tmp/ldb_sock'):
         self._s = socket(AF_UNIX, SOCK_STREAM)
         self._sock_path = sock_path
         self._s.bind(sock_path)
         self._s.listen(1)
+        self._current = -1
         self._conns = []
+
+    def current(self):
+        return self._conns[self._current] if self._current >= 0 else None
+
+    def next_context(self):
+        if self._current >= 0:
+            self._current = (self._current+1)%len(self._conns)
 
     def run(self):
         conn = self._s.accept()[0]
         conn.send('run');
         self._conns.append(Ldb(conn, self))
+        self._current = len(self._conns)-1
         return self._conns[-1]
 
     def start(self):
@@ -85,16 +127,20 @@ class LdbServer(object):
     def close_conn(self, conn):
         self._conns.remove(conn)
 
-    def __del__(self):
+    def teardown(self):
         for c in list(self._conns):
-            del c
-        self._lua.close()
+            c.teardown()
         self._s.close()
         os.remove(self._sock_path)
 
 if __name__ == '__main__':
     import sys
-    from IPython import embed
-    l = LdbServer() if len(sys.argv) < 2 else Ldb(sys.argv[1])
-    embed()
+    l = LdbServer()
+    console = Console(l)
+    try:
+        console.run()
+    except:
+        sys.exit(0)
+    finally:
+        l.teardown()
 
